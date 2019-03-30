be03788af7bce9512bcae7046f48e48ec372b1a3
hul <lukasz@hulanicki.pl>
2019-03-30 23:02:32 +0100

    step-2: redux form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-export const PostForm = () => (
-  <form >
-    <input type="text" />
-    <textarea />
+const createInput = render => ({ input, meta, label }) => (
+  <div className="FormField-input">
+    <label>{ label }</label>
+    {render(input)}
+    {meta.error && meta.touched && <span className="FormField-error">{meta.error}</span>}
+  </div>
+);
+
+const InputField = createInput(input => (
+  <input {...input}></input>
+));
+
+const TextAreaField = createInput(input => (
+  <textarea {...input}></textarea>
+));
+
+let PostForm = ({ handleSubmit }) => (
+  <form onSubmit={handleSubmit}>
+    <Field name="title" component={InputField} />
+    <Field name="text" component={TextAreaField} />
     <button type="submit">Zapisz</button>
   </form>
 );
+
+const validate = values => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Brak tytulu!";
+  }
+  if (!values.text) {
+    errors.text = "Brak tekstu!";
+  }
+  return errors;
+}
+
+PostForm = reduxForm({
+  form: 'post',
+  validate,
+  onSubmit: values => console.log(values)
+})(PostForm);
+
+export { PostForm };
